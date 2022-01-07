727a57ef413f97ea46f266f870e322f9220199f4
DucHT <ducht.personalemail@gmail.com>
2022-01-07 09:05:51 +0700

    Update useForm

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,15 +1,94 @@
 // libs
-import { useFormContext } from "react-hook-form";
+import {
+  FieldPath,
+  FieldPathValue,
+  FieldValues,
+  Path,
+  PathValue,
+  SetValueConfig,
+  UnpackNestedValue,
+  useFormContext,
+} from "react-hook-form";
 // types
 import { TAllFormValues } from "@/react-hook-form/types";
 
 type TPages = keyof TAllFormValues;
+type TFieldName<TFieldValues extends FieldValues> = FieldPath<TFieldValues>;
+type TFieldValue<TFieldValues extends FieldValues> = UnpackNestedValue<
+  FieldPathValue<TFieldValues, TFieldName<TFieldValues>>
+>;
+type TSetValues<TFieldValues extends FieldValues> = {
+  [key in TFieldName<TFieldValues>]?: TFieldValue<TFieldValues>;
+};
+type TEntryItem<TFieldValues extends FieldValues> = [
+  Path<TFieldValues>,
+  UnpackNestedValue<PathValue<TFieldValues, Path<TFieldValues>>>,
+];
 
 /**
- * useForm
- * @description Faster use by wrapping react-hook-form useFormContext with typescript
+ * useFormExtra
+ * @description Wrap react-hook-form useFormContext with typescript
+ * @description Override method trigger by validateForm
+ * @description Also return setValues fn for set multi values at once
  * @param _pageName
+ * @return validateForm
+ * @return setValues
+ * @return Rest react-hook-form useForm methods
+ * @see https://react-hook-form.com/api/useform
  */
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const useForm = <TPageName extends TPages>(_pageName: TPageName) =>
-  useFormContext<TAllFormValues[TPageName]>();
+export const useFormExtra = <TPageName extends TPages>(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  _pageName: TPageName,
+) => {
+  const { setValue, trigger, ...restFormProps } = useFormContext<
+    TAllFormValues[TPageName]
+  >();
+
+  return { validateForm, setValues, setValue, ...restFormProps };
+
+  /**
+   * setValues
+   * @param values
+   * @param options - SetValueConfig
+   * @example
+   * setValues({
+   *  fieldName1: "fieldValue1",
+   *  fieldName2: "fieldValue2",
+   * })
+   */
+  function setValues(
+    values: TSetValues<TAllFormValues[TPageName]>,
+    options?: SetValueConfig,
+  ) {
+    Object.entries(values).forEach((item) => {
+      const [name, value] = (item as unknown) as TEntryItem<
+        TAllFormValues[TPageName]
+      >;
+      setValue(name, value, options);
+    });
+  }
+
+  /**
+   * validateForm
+   * @param onPassed
+   * @param onFail
+   * @example
+   * validateForm({
+   *  onPassed: () => { console.log("Passed validates"); }
+   * })
+   */
+  async function validateForm({
+    onPassed,
+    onFail,
+  }: {
+    onPassed: () => void;
+    onFail?: () => void;
+  }) {
+    const isPassedValidate = await trigger();
+    if (isPassedValidate) {
+      onPassed();
+      return;
+    }
+    if (!isPassedValidate && !!onFail) onFail();
+  }
+};
