ad1369c213cfad5c4c4d565dd13c74dad8cb9426
DucHT <ducht.personalemail@gmail.com>
2022-03-14 15:56:16 +0700

    Add default theme

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -6,8 +6,44 @@ export const theme = createTheme({
       styleOverrides: {
         root: {
           background: "none",
+          padding: "10px 6px",
         },
       },
+    },
+    MuiAutocomplete: {
+      styleOverrides: {
+        root: {
+          display: "inline-block",
+          verticalAlign: "top",
+          width: 250,
+        },
+      },
+      defaultProps: {
+        size: "small",
+      },
+    },
+    MuiSelect: {
+      defaultProps: {
+        size: "small",
+      },
+    },
+    MuiFormControl: {
+      styleOverrides: {
+        root: {
+          // width: 250,
+          // @better-do: Write "margin: 4" here will creating BUGs!!
+          marginRight: 4,
+          marginLeft: 4,
+        },
+      },
+    },
+    MuiTextField: {
+      defaultProps: {
+        size: "small",
+      },
+    },
+    MuiCheckbox: {
+      defaultProps: { size: "small" },
     },
   },
 });
